951abde7fa5431439f61666ac5c399af7373c586
takokun <takokun.778@gmail.com>
2023-06-25 17:11:57 +0900

    first commit

--- handler/op/login_view.go
+++ handler/op/login_view.go
@@ -1,8 +1,9 @@
 package op
 
 import (
-	"log"
+	"fmt"
 	"net/http"
+	"text/template"
 
 	"github.com/morning-night-dream/oidc/pkg/openapi"
 )
@@ -12,9 +13,55 @@ func (op *OP) LoginView(
 	r *http.Request,
 	params openapi.OpLoginViewParams,
 ) {
-	// ログイン画面を表示する
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("cannot parse form:%s", err), http.StatusInternalServerError)
+		return
+	}
 
-	log.Printf("%+v", params)
-
-	w.Write([]byte(params.AuthRequestId))
+	var errMsg string
+	if err != nil {
+		errMsg = err.Error()
+	}
+	data := &struct {
+		ID    string
+		Error string
+	}{
+		ID:    params.AuthRequestId,
+		Error: errMsg,
+	}
+	if err = loginTmpl.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
+
+const tmp = `<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Login</title>
+	</head>
+	<body style="display: flex; align-items: center; justify-content: center; height: 100vh;">
+		<form method="POST" action="/login" style="height: 200px; width: 200px;">
+
+			<input type="hidden" name="id" value="{{.ID}}">
+
+			<div>
+				<label for="username">Username:</label>
+				<input id="username" name="username" style="width: 100%">
+			</div>
+
+			<div>
+				<label for="password">Password:</label>
+				<input id="password" name="password" style="width: 100%">
+			</div>
+
+			<p style="color:red; min-height: 1rem;">{{.Error}}</p>
+
+			<button type="submit">Login</button>
+		</form>
+	</body>
+</html>
+`
+
+var loginTmpl, _ = template.New("login").Parse(tmp)
