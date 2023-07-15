6bf6792d67718386b8fb89b0a4435d98d470b6fc
takokun <takokun.778@gmail.com>
2023-07-15 15:40:03 +0900

    first commit

--- model/id_token.go
+++ model/id_token.go
@@ -54,7 +54,7 @@ func (it IDToken) JWT(
 		"name":  it.Name,
 	}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	str, _ := token.SignedString([]byte(sign))
 
